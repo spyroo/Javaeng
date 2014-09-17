6fe0a9417cd0cc9ffb6a079dee44a652bde7a9ce
djohnson029 <declan.l.johnson@gmail.com>
2014-09-17 11:13:14 -0600

    Window now shows as borderless fullscreen Variables are now initialized properly

--- Game/src/game/Main.java
+++ Game/src/game/Main.java
@@ -1,4 +1,4 @@
-package org.sportsim.game.client;
+package game;
 
 public class Main {
 
---
+++ Game/src/game/Window.java
@@ -0,0 +1,38 @@
+package game;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+	
+	Dimension screenSize;
+	private int width;
+	private int height;
+	private static final long serialVersionUID = -4369730830015653927L;
+	
+	public Window() {
+		
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		width = (int) screenSize.getWidth();
+		height = (int) screenSize.getHeight();
+		System.out.println(width + ":" + height);
+		init();
+	}
+	
+	/**
+	 * Initialize the window
+	 */
+	public void init(){
+		setPreferredSize(new Dimension(width, height));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setUndecorated(true);
+		setVisible(true);
+		pack();
+	}
+	
+	
+
+}
