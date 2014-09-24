218d03734ee953487f1b97c1da93cfc758d04b90
Spyro <spyroo@users.noreply.github.com>
2014-09-24 09:23:10 -0500

    Update Engine.java

--- javaengine/src/org/engine/client/Engine.java
+++ javaengine/src/org/engine/client/Engine.java
@@ -4,14 +4,18 @@ public class Engine implements Runnable{
 	
 	private long lastFps;
 	private int fps;
+	private int workingFps;
 	private int maxFps;
 	
+	//-----DEBUG-----
 	public static void main(String[] args){
 		new Engine().startEngine();
 	}
+	//-----END DEBUG-----
 	
 	public Engine(){
 		fps = 0;
+		workingFps = 0;
 		maxFps = 60;
 		lastFps = getTime();
 	}
@@ -24,15 +28,20 @@ public class Engine implements Runnable{
 		
 		//Calculate the current fps
 		if(getTime() - lastFps > 1000){
-			fps = 0;
+			fps = workingFps;
+			workingFps = 0;
 			lastFps += 1000;
+			System.out.println("FPS: " + fps);
 		}
-		if(fps > maxFps)
-			return;
-		fps++;
-		System.out.println("FPS: " + fps);
+		workingFps++;
+		
 	}
 	
+	/**
+	 * Returns the current time in milliseconds based on the System nanTime 
+	 * @return The current time in mills
+	 * 
+	 **/
 	private long getTime(){
 		return System.nanoTime() / 1000000;
 	}
@@ -44,10 +53,18 @@ public class Engine implements Runnable{
 		}
 	}
 	
+	/**
+	 * Returns the current frames per second the application is running at.
+	 * @return The current frames per second
+	 **/
 	public int getCurrentFps(){
 		return fps;
 	}
 	
+	/**
+	 * Starts the engine, creating a new thread and invoking it. 
+	 * 
+	 **/
 	public void startEngine(){
 		new Thread(this).start();
 	}
