2c9409ad302a51871e57c69efacf61c39473eaa7
Prestonium <pmcinelly783@gmail.com>
2014-09-29 12:22:49 -0600

    Progress on S0UND

--- javaengine/src/org/engine/client/Sound.java
+++ javaengine/src/org/engine/client/Sound.java
@@ -2,30 +2,22 @@ package org.engine.client;
 import java.io.InputStream;
 
 import javax.sound.sampled.AudioInputStream;
+import java.applet.*;
+import java.net.*;
 
 public class Sound implements Runnable {
-	private String SOUND_FILENAME;
+
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		playSound();
-	}
-	private void playSound() 
-	{
-	  try
-	  {
-	    // get the sound file as a resource out of my jar file;
-	    // the sound file must be in the same directory as this class file.
-	    // the input stream portion of this recipe comes from a javaworld.com article.
-	    InputStream inputStream = getClass().getResourceAsStream(SOUND_FILENAME);
-	    AudioInputStream audioStream = new AudioInputStream(inputStream);
-	    AudioPlayer.player.start(audioStream);
-	  }
-	  catch (Exception e)
-	  {
-	    // a special way i'm handling logging in this application
-	    if (debugFileWriter!=null) e.printStackTrace(debugFileWriter);
-	  }
+		try {
+			AudioClip clip = Applet.newAudioClip(
+			new URL(Place Location Here));
+			clip.play();
+			} catch (MalformedURLException murle) {
+			System.out.println(murle);
+			}	
 	}
 }
 
