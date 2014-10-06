15bc74dfa03695e61023c520eae9aa8bb028749f
Kyle Surowiec <kylesurowiec@me.com>
2014-10-06 09:13:11 -0500

    Fixed duplicate code error

--- javaengine/src/org/javaeng/core/RenderFrame.java
+++ javaengine/src/org/javaeng/core/RenderFrame.java
@@ -10,59 +10,67 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-public class RenderFrame extends JPanel implements MouseListener, MouseMotionListener, KeyListener{
+public class RenderFrame extends JPanel implements MouseListener,
+		MouseMotionListener, KeyListener {
 
 	private static final long serialVersionUID = 3943753884244498773L;
 	private ArrayList<Entity> entities;
 	private static ArrayList<Sound> soundQueue;
 	private static boolean canAddSoundToQueue;
 	private UserInputListener userInputListener;
-	
-	public RenderFrame(){
-		entities= new ArrayList<Entity>();
+
+	public RenderFrame() {
+		entities = new ArrayList<Entity>();
 		soundQueue = new ArrayList<Sound>();
 		canAddSoundToQueue = true;
 	}
-	
-	public void addUserInputListener(UserInputListener userInputListener){
+
+	public void addUserInputListener(UserInputListener userInputListener) {
 		this.userInputListener = userInputListener;
 	}
-	
+
 	/**
-	 * Updates the render frame, redrawing all of the entities and calling their update method
+	 * Updates the render frame, redrawing all of the entities and calling their
+	 * update method
 	 * 
 	 */
-	public synchronized void updateFrame(int delta){
 	
-	public synchronized void updateFrame(){
-		for(Entity e : entities){
+	public synchronized void updateFrame(int delta) {
+		for (Entity e : entities) {
 			e.update(delta);
 		}
 		canAddSoundToQueue = false;
-		for(Sound s : soundQueue){
+		for (Sound s : soundQueue) {
 			new Thread(s).start();
 		}
 		soundQueue.clear();
 		canAddSoundToQueue = true;
 		repaint();
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		for(Entity e : entities){
-			if(e.getTexture() != null)
-				g.drawImage(e.getTexture().getImage().getScaledInstance(e.getBoundingBox().getWidth(), e.getBoundingBox().getHeight(), 0), e.getBoundingBox().getX(), e.getBoundingBox().getY(), null);
+		for (Entity e : entities) {
+			if (e.getTexture() != null)
+				g.drawImage(
+						e.getTexture()
+								.getImage()
+								.getScaledInstance(
+										e.getBoundingBox().getWidth(),
+										e.getBoundingBox().getHeight(), 0), e
+								.getBoundingBox().getX(), e.getBoundingBox()
+								.getY(), null);
 		}
-		
+
 	}
-	
-	public synchronized void addEntity(Entity e){
+
+	public synchronized void addEntity(Entity e) {
 		entities.add(e);
 	}
-	
-	public synchronized static boolean addSoundToQueue(Sound s){
-		if(canAddSoundToQueue){
+
+	public synchronized static boolean addSoundToQueue(Sound s) {
+		if (canAddSoundToQueue) {
 			soundQueue.add(s);
 			return true;
 		}
@@ -71,8 +79,8 @@ public class RenderFrame extends JPanel implements MouseListener, MouseMotionLis
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		for(Entity ent : entities){
-			if(ent.getBoundingBox().isOverlapping(e.getX(), e.getY())){
+		for (Entity ent : entities) {
+			if (ent.getBoundingBox().isOverlapping(e.getX(), e.getY())) {
 				ent.onClicked();
 			}
 		}
@@ -80,63 +88,63 @@ public class RenderFrame extends JPanel implements MouseListener, MouseMotionLis
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mousePressed(e);
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mouseReleased(e);
 		}
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mouseEntered(e);
 		}
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mouseExited(e);
-		}		
+		}
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mouseDragged(e);
-		}		
+		}
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.mouseMoved(e);
 		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		for(Entity ent : entities){
+		for (Entity ent : entities) {
 			ent.keyTyped(e);
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(userInputListener != null)
+		if (userInputListener != null)
 			userInputListener.addKeyDown(e.getKeyCode());
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(userInputListener != null)
+		if (userInputListener != null)
 			userInputListener.removeKeyDown(e.getKeyCode());
 	}
-	
+
 }
